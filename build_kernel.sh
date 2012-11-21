e62ad6bca403f741054e1d5ba1438aae06efe641
yank555-lu <yank555.lu@gmail.com>
2012-11-21 20:07:47 +0100

    boot.img build script & ramdisk

---
+++ build_kernel.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
+export USE_SEC_FIPS_MODE=true
+
+if [ "${1}" != "" ];then
+  export KERNELDIR=`readlink -f ${1}`
+fi
+
+RAMFS_TMP="/home/yank555-lu/temp/tmp/ramfs-source-sgs3"
+
+. $KERNELDIR/.config
+
+echo ".............................................................Building new ramdisk............................................................."
+#remove previous ramfs files
+rm -rf $RAMFS_TMP
+rm -rf $RAMFS_TMP.cpio
+rm -rf $RAMFS_TMP.cpio.gz
+#copy ramfs files to tmp directory
+cp -ax $RAMFS_SOURCE $RAMFS_TMP
+#clear git repositories in ramfs
+find $RAMFS_TMP -name .git -exec rm -rf {} \;
+#remove empty directory placeholders
+find $RAMFS_TMP -name EMPTY_DIRECTORY -exec rm -rf {} \;
+rm -rf $RAMFS_TMP/tmp/*
+#remove mercurial repository
+rm -rf $RAMFS_TMP/.hg
+cd $RAMFS_TMP
+find | fakeroot cpio -H newc -o > $RAMFS_TMP.cpio 2>/dev/null
+ls -lh $RAMFS_TMP.cpio
+gzip -9 $RAMFS_TMP.cpio
+
+echo "...............................................................Compiling kernel..............................................................."
+cd $KERNELDIR
+make -j6 || exit 1
+
+echo ".............................................................Making new boot image............................................................"
+./mkbootimg --kernel $KERNELDIR/arch/arm/boot/zImage --ramdisk $RAMFS_TMP.cpio.gz --board smdk4x12 --base 0x10000000 --pagesize 2048 --ramdiskaddr 0x11000000 -o $KERNELDIR/boot.img
+
+echo ".....................................................................done....................................................................."
+find . -name "boot.img"
+find . -name "*.ko"
+
