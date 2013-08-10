8d4ed49bef61439a43b3db8f6ceec4d0c3a341a5
yank555-lu <yank555.lu@gmail.com>
2013-08-10 16:53:07 +0200

    Touch Wake : bump to v1.1a (Yank555.lu)
    - fixed touch wake disbaled as soon as proxy sensor was enabled, no matter if proximity was detected or not (my derp, introduced in 1.1)
    - fixed proximity state change detection (is in cm36651.c, not ssp_input.c, which is not compiled for i9300)
    - removed "off hook" detection, it was relying on a constant (PSTN_ON_HOOK, defined in telephony.h), so made no sense to keep it

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -27,7 +27,6 @@
 #include <linux/mutex.h>
 #include <linux/rcupdate.h>
 #include "input-compat.h"
-#include <linux/telephony.h>
 
 #ifdef CONFIG_TOUCH_WAKE
 #include <linux/touch_wake.h>
@@ -290,7 +289,7 @@ static int input_get_disposition(struct input_dev *dev,
 		if (is_event_supported(code, dev->keybit, KEY_MAX) &&
 		    !!test_bit(code, dev->key) != value) {
 #ifdef CONFIG_TOUCH_WAKE
-      if (code == KEY_POWER && !device_is_suspended() && !PSTN_ON_HOOK) {
+      if (code == KEY_POWER && !device_is_suspended()) {
         if (value == 1) {
           powerkey_pressed();
         }
